ad349509639eb64112d92c8b205662621f5d2bc4
Ryan Scharfer <rscharfer@gmail.com>
2021-11-13 17:10:05 -0800

    done with select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,58 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Presentation = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+`
+const NativeSelect = styled.select`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  opacity: 0;
+`
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  color: ${COLORS.gray700};
+  &:hover {
+    color: black;
+  }
+
+  
+
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin-top: auto;
+  margin-bottom: auto;
+  right: 10px;
+  height: 24px;
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper tabIndex="0">
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <Presentation>
+        {displayedValue}
+      </Presentation>
+      <IconWrapper>
+        <Icon id='chevron-down' size={24}/>
+      </IconWrapper>
+      
+    </Wrapper>
   );
 };
 
